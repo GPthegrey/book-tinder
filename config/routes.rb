c53088fb1c34f4cb5ed37957e7db57ad4b2d604a
Guido Palmili <guidopalmili@gmail.com>
2024-04-06 17:03:13 -0300

    footer created, form for books created, terms of use added, avatar, card created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get 'pages/terminos_de_uso', to: "pages#terminos_de_uso", as: "terminos_de_uso"
   resources :users, only: [:show]
   resources :books
   get 'mybooks', to: 'books#my_books', as: :my_books
